97dfbec0040ec63db71366719bfb975876841f66
DkNabeel <143883787+DkNabeel@users.noreply.github.com>
2026-04-23 13:28:30 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,10 @@
 import streamlit as st
+import re
+
+def clean_text(text):
+    text = text.lower()
+    text = re.sub(r'[^a-zA-Z ]', '', text)
+    return text
 
 st.title("📰 Fake News Detector")
 
@@ -10,10 +16,14 @@ image = st.file_uploader("Upload image")
 
 if st.button("Check"):
     if text:
-        st.write("Text input received")
+        cleaned = clean_text(text)
+        st.write("Cleaned Text:", cleaned)
+
     elif link:
         st.write("Link input received")
+
     elif image:
         st.write("Image input received")
+
     else:
         st.write("No input provided")
