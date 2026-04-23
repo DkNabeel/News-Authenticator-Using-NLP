aaae35109f29ea070333dc2cc98095899fb5efc4
DkNabeel <143883787+DkNabeel@users.noreply.github.com>
2026-04-23 15:03:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -45,13 +45,10 @@ text = st.text_area("Enter text")
 link = st.text_input("Enter link")
 image = st.file_uploader("Upload image")
 
-if st.button("Check"):if text:cleaned = clean_text(text)
-
-    vector = vectorizer.transform([cleaned])
-
-    prediction = model.predict(vector)[0]
-
-    st.write("Result:", prediction)
-
-else:
-    st.write("Enter some text")
+if st.button("Check"):
+    if text:cleaned = clean_text(text)
+        vector = vectorizer.transform([cleaned])
+        prediction = model.predict(vector)
+        st.write("Result:", prediction)
+    else:
+        st.write("Enter some text")
