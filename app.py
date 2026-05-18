78238a13754a7fee4ea1b24e357d826e3233e94e
DkNabeel <143883787+DkNabeel@users.noreply.github.com>
2026-05-18 12:52:12 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,11 +3,22 @@ import streamlit as st
 from utils import clean_text
 from model import load_model
 
-vectorizer, model = load_model()
+# fast loading
+@st.cache_resource
+def get_model():
+    return load_model()
+
+vectorizer, model = get_model()
 
 st.title("Fake News Detector")
 
-text = st.text_area("Enter News Text")
+st.write("Enter news using text, link, or image")
+
+text = st.text_area("Enter Text")
+
+link = st.text_input("Enter Link")
+
+image = st.file_uploader("Upload Image")
 
 if st.button("Check"):
 
@@ -19,7 +30,18 @@ if st.button("Check"):
 
         prediction = model.predict(vector)[0]
 
-        st.write("Prediction:", prediction)
+        st.subheader("Result")
+
+        if prediction == "Real":
+            st.success("Real News")
+        else:
+            st.error("Fake News")
+
+    elif link:
+        st.info("Link feature coming soon")
+
+    elif image:
+        st.info("Image feature coming soon")
 
     else:
-        st.write("Enter some text")
+        st.warning("Please enter some input")
